a062253af8007d5f37df0b82ef1a5031d2b2db7f
Alice GG <hi@alicegg.tech>
2024-04-30 17:31:21 +0200

    Naive multithreading

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"image/color"
 	"log"
 	"os"
+        "sync"
 
 	"github.com/hschendel/stl"
 )
@@ -15,7 +16,7 @@ func readSTL() *stl.Solid {
         log.Panicf("Error while reading stl file: %v", err)
     }
 
-    solid.ScaleLinearDowntoSizeBox(stl.Vec3{0.50, 0.50, 0.50})
+    solid.ScaleLinearDowntoSizeBox(stl.Vec3{0.80, 0.80, 0.80})
     solid.Rotate(stl.Vec3{0,0,0}, stl.Vec3{0,1,0}, stl.HalfPi + stl.QuarterPi)
     solid.Rotate(stl.Vec3{0,0,0}, stl.Vec3{1,0,0}, stl.QuarterPi)
     solid.Rotate(stl.Vec3{0,0,0}, stl.Vec3{0,0,1}, stl.HalfPi)
@@ -31,32 +32,39 @@ func main() {
 
     solid := readSTL()
     image := newImage(width, height)
+    var wg sync.WaitGroup
 
     for i := range width {
         for j := range height {
-            image.Set(i, height-j, color.White)
+            wg.Add(1)
 
-            distance := float32(1)
-            intersects := false
-            ray := castRay(
-                float32(i) / (float32(width) - 1),
-                float32(j) / (float32(height) - 1),
-            )
-            for _, triangle := range solid.Triangles {
-                t, ok := ray.intersect(triangle)
-                if ok {
-                    if t < distance  {
-                        distance = t
+            go func(i, j int) {
+                defer wg.Done()
+                image.Set(i, height-j, color.White)
+
+                distance := float32(1)
+                intersects := false
+                ray := castRay(
+                    float32(i) / (float32(width) - 1),
+                    float32(j) / (float32(height) - 1),
+                )
+                for _, triangle := range solid.Triangles {
+                    t, ok := ray.intersect(triangle)
+                    if ok {
+                        if t < distance  {
+                            distance = t
+                        }
+                        intersects = true
                     }
-                    intersects = true
                 }
-            }
 
-            if intersects {
-                image.Set(i, height-j, getBlue(distance))
-            }
+                if intersects {
+                    image.Set(i, height-j, getBlue(distance))
+                }
+            }(i, j)
         }
     }
 
+    wg.Wait()
     writePNG(image)
 }
