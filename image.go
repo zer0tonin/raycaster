d9d2ddec6c4269ed262c40a47bc4f334e1d0e4bd
Alice GG <hi@alicegg.tech>
2024-04-30 17:31:05 +0200

    Image tweaking

--- image.go
+++ image.go
@@ -27,8 +27,8 @@ func writePNG(image image.Image) {
 
 func getBlue(distance float32) color.RGBA {
     return color.RGBA{
-        R: uint8(float32(255) * (1 - distance)),
-        G: uint8(float32(255) * (1 - distance)),
+        R: uint8(float32(200) * (1 - distance)),
+        G: uint8(float32(200) * (1 - distance)),
         B: uint8(255),
         A: 255,
     }
